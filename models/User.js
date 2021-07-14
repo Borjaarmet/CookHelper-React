6444ef85aaae813f919db88d376e2e654e950849
Borjaarmet <borjaarmet@gmail.com>
2021-07-14 09:52:34 +0200

    modify user model

--- models/User.js
+++ models/User.js
@@ -4,9 +4,27 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema(
 	{
-		username: { type: String, required: true, unique: true },
-		hashedPassword: { type: String, required: true },
-		email: { type: String, required: true },
+		username: {
+			type: String,
+			trim: true,
+			required: [true, 'Username is required'],
+			unique: true,
+		},
+		email: {
+			type: String,
+			required: [true, 'email is required'],
+			match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.'],
+			unique: true,
+			lowercase: true,
+			trim: true,
+		},
+		hashedPassword: {
+			type: String,
+			required: [true, 'Password is required'],
+		},
+		favouriteRecipes: [{ type: Schema.Types.ObjectId, ref: 'Recipe' }],
+		nationality: { type: String },
+		age: { type: Number },
 	},
 	{
 		timestamps: {
