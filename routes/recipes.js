39bab7b03ece00d458f324f159fc914125445c8b
Borjaarmet <borjaarmet@gmail.com>
2021-07-14 12:26:47 +0200

    fix the delete route to user route

--- routes/recipes.js
+++ routes/recipes.js
@@ -48,28 +48,21 @@ router.post('/', checkIfLoggedIn, (req, res) => {
 	});
 });
 
-router.put('/:id', checkIfLoggedIn, (req, res) => {
-	const { id } = req.params;
-	const { recipeName, difficulty, TimeToCook, ingredientsList, Steps, videoLink } = req.body;
-	Recipe.findByIdAndUpdate(
-		id,
-		{ recipeName, difficulty, TimeToCook, ingredientsList, Steps, videoLink },
-		{ new: true }
-	).then(Recipeupdated => {
-		// if (Recipeupdated === null) {
-		// 	return res.status(404).json({ error: 'Recipe not founded' });
-		// }
-		return res.json({
-			updatedRecipe: Recipeupdated,
-		});
-	});
-});
-
-router.delete('/:id', checkIfLoggedIn, (req, res) => {
-	const { id } = req.params;
-	Recipe.findByIdAndDelete(id).then(deletedRecipe => {
-		res.json({ deleted: deletedRecipe });
-	});
-});
+// router.put('/:id', checkIfLoggedIn, (req, res) => {
+// 	const { id } = req.params;
+// 	const { recipeName, difficulty, TimeToCook, ingredientsList, Steps, videoLink } = req.body;
+// 	Recipe.findByIdAndUpdate(
+// 		id,
+// 		{ recipeName, difficulty, TimeToCook, ingredientsList, Steps, videoLink },
+// 		{ new: true }
+// 	).then(Recipeupdated => {
+// 		// if (Recipeupdated === null) {
+// 		// 	return res.status(404).json({ error: 'Recipe not founded' });
+// 		// }
+// 		return res.json({
+// 			updatedRecipe: Recipeupdated,
+// 		});
+// 	});
+// });
 
 module.exports = router;
