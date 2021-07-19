4d0bf8526a88792cd1e28e9e52c8cf0a8a37b590
Borjaarmet <borjaarmet@gmail.com>
2021-07-19 12:54:36 +0200

    rename some routes

--- routes/user.js
+++ routes/user.js
@@ -15,10 +15,10 @@ const router = express.Router();
 
 router.get('/', checkIfLoggedIn, getUserInSession);
 router.get('/:id/profile', checkIfLoggedIn, getUserProfile);
-router.put('/profile', checkIfLoggedIn, getUpdatedProfile);
-router.get('/profile/create', checkIfLoggedIn, getUserCreatedRecipes);
-router.post('/profile/create', checkIfLoggedIn, createdRecipe);
-router.delete('/profile/:id', checkIfLoggedIn, deletedRecipeFromCreatedList);
+router.put('/:id/profile', checkIfLoggedIn, getUpdatedProfile);
+router.get('/create', checkIfLoggedIn, getUserCreatedRecipes);
+router.post('/create', checkIfLoggedIn, createdRecipe);
+router.delete('/create/:id', checkIfLoggedIn, deletedRecipeFromCreatedList);
 router.get('/favourites', checkIfLoggedIn, getUserFavouritesRecipes);
 router.delete('/favourites/:id', checkIfLoggedIn, deletedRecipeFromFav);
 
